93ca74a5503806b29919e700627b86d4aac1c7b9
sunchaser <anika.mclean@gmail.com>
2022-05-19 17:06:18 -0400

    add user login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,13 +30,22 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      
+
       res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
